c24055df26dd5f3920c9d4aace57145c64a5fb5b
Arun Sittampalam <arun@sittampalam.ch>
2016-11-28 16:21:28 +0100

    Saving model to filesystem.

--- medtextcollector_pipeline.py
+++ medtextcollector_pipeline.py
@@ -4,10 +4,14 @@ from vectorizer import simple_bag_of_words
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.classify.positivenaivebayes import PositiveNaiveBayesClassifier
 from pprint import pprint
-
-
+import os
+import pickle
 
 if __name__ == '__main__':
+    print('Preparing base folder... ')
+    if not os.path.exists(__CONFIG__['base-folder']):
+        os.makedirs(__CONFIG__['base-folder'])
+        
     print('Loading data ...')
     positive_documents = ['Dies ist ein Testdokument.', 'Dies ist auch ein Testdkument.', 'Und noch ein Testdokument']
     unlabeled_documents = ['Lorem ipsum.', 'Dolor sit amet.', 'Blablaba bla bla.']
@@ -27,5 +31,8 @@ if __name__ == '__main__':
     
     classifier = PositiveNaiveBayesClassifier.train(positive_vectorized_documents, unlabeled_vectorized_documents)
     
+    print("Saving model to " + os.path.join(__CONFIG__['base-folder'], 'pos_naive_bayes_model.pickle') + " ...")    
+    with open(os.path.join(__CONFIG__['base-folder'], 'pos_naive_bayes_model.pickle'), 'wb') as file:
+        pickle.dump(classifier, file)
+        
     pprint(classifier.prob_classify(simple_bag_of_words(tokenizer.tokenize("Nochmal ein Testdokument."))).prob(True))    
-    
