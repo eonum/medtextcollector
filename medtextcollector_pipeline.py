c590d6188f532893a8c8715607e9218242ff0b69
Arun Sittampalam <arun@sittampalam.ch>
2016-11-28 13:11:38 +0100

    Using bag of words.

--- medtextcollector_pipeline.py
+++ medtextcollector_pipeline.py
@@ -1,4 +1,27 @@
 from load_config import __CONFIG__
-from tokenizer import GermanStemmer
+from tokenizer import SimpleGermanTokenizer
+from sklearn.feature_extraction.text import CountVectorizer
+from pprint import pprint
+
 if __name__ == '__main__':
-    pass
+    print("Loading data ...")
+    documents = ["This is a test document.", "This also is a document meant for testing purposes.", "And another one of those."] 
+    
+    print("Tokenizing ...")
+    tokenizer = SimpleGermanTokenizer()
+    tokenized_documents = []
+    for document in documents:
+        tokenized_documents.append(" ".join(tokenizer.tokenize(document)))
+    
+    print("Vectorizing ...")
+    vectorizer = CountVectorizer(analyzer = "word",   \
+                             tokenizer = None,    \
+                             preprocessor = None, \
+                             stop_words = None,   \
+                             max_features = 5000) 
+    vectorized_documents = vectorizer.fit_transform(tokenized_documents)
+    
+    if __CONFIG__['debug']:
+        vocab = vectorizer.get_feature_names()
+        print("Vocabulary: ")
+        pprint(vocab)
