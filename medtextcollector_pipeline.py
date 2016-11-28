d840b928e1dbfc349b9d65c3b7a5ed63cf1f91ad
Arun Sittampalam <arun@sittampalam.ch>
2016-11-28 16:02:24 +0100

    Using PositiveNaiveBayesClassifier.

--- medtextcollector_pipeline.py
+++ medtextcollector_pipeline.py
@@ -1,27 +1,36 @@
 from load_config import __CONFIG__
 from tokenizer import SimpleGermanTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
+from nltk.classify.positivenaivebayes import PositiveNaiveBayesClassifier
 from pprint import pprint
 
+def bag_of_words(words):
+    return dict([(word, True) for word in words])
+
 if __name__ == '__main__':
-    print("Loading data ...")
-    documents = ["This is a test document.", "This also is a document meant for testing purposes.", "And another one of those."] 
+    print('Loading data ...')
+    positive_documents = ['Dies ist ein Testdokument.', 'Dies ist auch ein Testdkument.', 'Und noch ein Testdokument']
+    unlabeled_documents = ['Lorem ipsum.', 'Dolor sit amet.', 'Blablaba bla bla.']
     
-    print("Tokenizing ...")
+    print('Tokenizing ...')
     tokenizer = SimpleGermanTokenizer()
-    tokenized_documents = []
-    for document in documents:
-        tokenized_documents.append(" ".join(tokenizer.tokenize(document)))
+    positive_tokenized_documents = []
+    unlabeled_tokenized_documents = []
+    for document in positive_documents:
+        positive_tokenized_documents.append(tokenizer.tokenize(document))
+    for document in unlabeled_documents:
+        unlabeled_tokenized_documents.append(tokenizer.tokenize(document))
     
-    print("Vectorizing ...")
-    vectorizer = CountVectorizer(analyzer = "word",   \
-                             tokenizer = None,    \
-                             preprocessor = None, \
-                             stop_words = None,   \
-                             max_features = 5000) 
-    vectorized_documents = vectorizer.fit_transform(tokenized_documents)
+    print('Vectorizing ...')
+    positive_vectorized_documents = [bag_of_words(tokens) for tokens in positive_tokenized_documents]
+    unlabeled_vectorized_documents = [bag_of_words(tokens) for tokens in unlabeled_tokenized_documents]
     
     if __CONFIG__['debug']:
         vocab = vectorizer.get_feature_names()
-        print("Vocabulary: ")
+        print('Vocabulary: ')
         pprint(vocab)
+    
+    classifier = PositiveNaiveBayesClassifier.train(positive_vectorized_documents, unlabeled_vectorized_documents)
+    
+    pprint(classifier.prob_classify(bag_of_words(tokenizer.tokenize("Nochmal ein Testdokument."))).prob(True))    
+    
