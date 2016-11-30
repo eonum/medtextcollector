344bdc21cedeefaf6dfbb22cb329432de2e665a7
Samuel Jaggi <sam@mars.home>
2016-11-30 13:40:18 +0100

    Was the same script as clan_xlm.rb Code doesn't work proberly at the moment Thre was a mistake during copying, so that the script content was the same.

--- scripts/scan_categories.rb
+++ scripts/scan_categories.rb
@@ -1,26 +1,19 @@
-require 'nokogiri'
-
-@doc = Nokogiri::XML(File.open("test_top_file_1000.xml"))
-@doc.encoding = 'UTF-8'
-@doc = @doc.remove_namespaces!
-
-# this variable stores the content of the '<text></text>' tag from the wikipedia xml.
-# in the '<text></text>' tag is the usefull content of a wikipedia-site
-@wikipedia_text_array = @doc.xpath("//text")
-
-@wikipedia_text_cleaned_array = Array.new
-
-# This Array contains all the expressions which should not appear in the plaintext 
-array_clean_expressions = Array.new
-array_clean_expressions = ["\n", %q[<text xml:space=\"preserve\"], "<\text>", "'''''", "'''", "''", "{{", "}}", "~~~~", "<s>", "<\s>", "<\s>", "<u>", "<\u>", "<!--", "-->", "======", "=====", "====", "===", "==", "**", "##", "::::", ":::", "::", "&lt;!--", "--&gt;"]
-
-@wikipedia_text_array.each do |i|
-  wikipedia_text = i.to_s
-  puts wikipedia_text.is_a?(String)
-
-  array_clean_expressions.each do |exp|
-    wikipedia_text = wikipedia_text.gsub(exp, " ")
+arr = []
+line_num=0
+text=File.open('../data/dewiki-latest-category.sql').read
+text.gsub!(/\r\n?/, "\n")
+text.each_line do |line|
+	
+  arr << line.scan(/'([^']*)'/)
+  puts arr
+end
+arr.compact
+File.open("test.txt", "w+") do |f|
+  arr.each do |a|
+    a.to_s
+    if a[].include?('Sport')
+      puts a		
+      f.puts(a)
+    end
   end
-
-  @wikipedia_text_cleaned_array << wikipedia_text
 end
