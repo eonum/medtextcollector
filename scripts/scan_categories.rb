df3e1fe870072807dee4aee2bcf11c591cf0ba88
Samuel Jaggi <sam@mars.home>
2016-11-30 15:26:31 +0100

    Improve scan_categories.rb, works now

--- scripts/scan_categories.rb
+++ scripts/scan_categories.rb
@@ -3,17 +3,19 @@ line_num=0
 text=File.open('../data/dewiki-latest-category.sql').read
 text.gsub!(/\r\n?/, "\n")
 text.each_line do |line|
-	
-  arr << line.scan(/'([^']*)'/)
-  puts arr
+  line.scan(/'([^']*)',/).each{|w| arr << w}
 end
-arr.compact
-File.open("test.txt", "w+") do |f|
-  arr.each do |a|
-    a.to_s
-    if a[].include?('Sport')
-      puts a		
-      f.puts(a)
+File.open("../data/medizin_category.txt", "w+") do |f|
+  arr.each do |element| 
+    if element.to_s.downcase.gsub(/\s+/, '').include?("medizin")
+      f.puts(element)
+    end
+  end
+end
+File.open("../data/gesundheit_category.txt", "w+") do |f|
+  arr.each do |element|
+    if element.to_s.downcase.gsub(/\s+/, '').include?("gesundheit")
+      f.puts(element)
     end
   end
 end
