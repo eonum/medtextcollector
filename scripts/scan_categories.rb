6ee358acda69b5b136575a0994ce825a6ad25f9e
Samuel Jaggi <sam@mars.home>
2017-02-17 17:05:06 +0100

    Better category scanner (more hits)

--- scripts/scan_categories.rb
+++ scripts/scan_categories.rb
@@ -5,17 +5,15 @@ text.gsub!(/\r\n?/, "\n")
 text.each_line do |line|
   line.scan(/'([^']*)',/).each{|w| arr << w}
 end
-File.open("../data/medizin_category.txt", "w+") do |f|
+
+key_words = ["Medizin nach Zeit","Mediziner","Alternativmedizin","Medizinische Bildung","Biomedizin","Diagnostik","Medizinische Dokumentation","Epidemiologie","Medizinethik","Ethnomedizin","Evidenzbasierte Medizin","Exkrement","Medizin in Film und Fernsehen","Medizinisches Gebiet","Medizingeschichte","Heilberuf","Medizininformatik","Japanische Medizin","Krankheit","Krankheit als Thema","Medizinische Klassifikation","Naturheilkunde","Organ als Thema","Organisation Medizin","Medizinpreis","Medizinrecht","Sachliteratur Medizin","Sexualmedizin","Medizinstatistik","Strahlenbiologie","Medizintechnik","Therapie","Veterinärmedizi","Medizinische Vorsorg","Zahnmedizin", "medizin", "gesundheit"]
+
+File.open("../data/categories", "w+") do |f|
   arr.each do |element| 
-    if element.to_s.downcase.gsub(/\s+/, '').include?("medizin")
+    key_words.each do |kword|
+      if element.to_s.downcase.gsub(/\s+/, '').include?(kword)
       f.puts(element)
-    end
-  end
-end
-File.open("../data/gesundheit_category.txt", "w+") do |f|
-  arr.each do |element|
-    if element.to_s.downcase.gsub(/\s+/, '').include?("gesundheit")
-      f.puts(element)
+      end
     end
   end
 end
