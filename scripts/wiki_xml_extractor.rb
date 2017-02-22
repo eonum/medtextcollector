9c26cae1cf94440fd45bc7416769e4b878ad654c
Samuel Jaggi <samuel.jaggi@eonum.ch>
2017-02-22 09:44:03 +0100

    Major changes in the scrip, works now

--- scripts/wiki_xml_extractor.rb
+++ scripts/wiki_xml_extractor.rb
@@ -7,75 +7,97 @@ require 'nokogiri'
 # The text of each page is searched with xpath and its title is added on the first line as
 # an html comment: this is required by pagefromfile.py.
 #
+counter = 1
 Dir.glob("../data/dewiki-latest-pages-articles*").each do |filename|
   input = Nokogiri::XML(File.new(filename), nil, 'UTF-8')
-
   puts filename.to_s  # prints the name of each .xml file
 
-  File.open("out_" + filename + ".wiki", 'w') {|f|
-    input.xpath("//xmlns:text").each {|n|
+  File.open("../data/out_dewiki-latest-pages-articles" + "#{counter}" + ".wiki", "w") do |f|
+    input.xpath("//xmlns:text").each do |n|
       pptitle = n.parent.parent.at_css "title" # searching for the title
       title = pptitle.content
       f.puts "\n{{-start-}}<!--'''" << title.to_s << "'''-->" << n.content  << "\n{{-stop-}}"
-    }
-  }
-end
-
-#### this part cleans the extracted text. Script is in progress
-
-file_content = File.open("out_dewiki-latest-pages-articles1.xml-p000000001p000425449.wiki", "r:UTF-8", &:read); nil
-file_content = file_content.gsub(/\n/, ""); nil
-content = []
-file_content.scan(/{{-start-}}(.*?){{-stop-}}/).each do |hit|
-  content << hit.to_s
-end; nil
-
-File.open("test", "w+") do |f|
-  content.each { |element| f.puts(element) }
-end; nil
-
-
-categories = []
-File.readlines('gesundheit_category.txt').each do |line|
-  line = line.gsub(/\n/, "")
-  categories << line
-end; nil
-
-matches = []
-
-content.each do |element|
-  catch :categoryfound do
-    categories.each do |category|
-      if element.include?("[[Kategorie:#{category}]]")
-        matches << element
-      end
     end
   end
-end; nil
+  counter += 1
+end
 
-matches.map! do |element|
-  element = element.gsub(/<ref(.*?)<\/ref>/, "")
-  element = element.gsub(/== Literatur ==(.*?)==/, "")
-  element = element.gsub(/== Weblinks ==(.*?)==/, "")
-  element = element.gsub(/== Einzelnachweise ==(.*?)==/, "")
-  element = element.gsub(/\[\[/, "")
-  element = element.gsub(/\]\]/, "")
-  element = element.gsub(/<ref(.*?)>/, "")
-  element = element.gsub(/\'\'{{lang(.*?)\'\'/, "")
-  element = element.gsub(/{{lang(.*?)}}/, "")
-  element = element.gsub(/=====/, "")
-  element = element.gsub(/====/, "")
-  element = element.gsub(/===/, "")
-  element = element.gsub(/==/, "")
-  element = element.gsub(/'''''/, "")
-  element = element.gsub(/''''/, "")
-  element = element.gsub(/'''/, "")
-  element = element.gsub(/''/, "")
-  element = element.gsub(/\*\*\*\*\*/, "")
-  element = element.gsub(/\*\*\*\*/, "")
-  element = element.gsub(/\*\*\*/, "")
-  element = element.gsub(/\*\*/, "")
-  element = element.gsub(/\{\|(.*?)\|\}/, "")
-  element = element.gsub(/\{\{(.*?)\}\}/, "")
-  element = element.gsub(/&nbsp\;/, "")
-end;nil
+
+#### this part cleans the extracted text. Script is in progress
+# nil in this section are because otherwise it always shows a output in the terminal and this might slow down the script
+
+counter = 1                                                             # reset counter to 1
+
+Dir.glob("../data/out_dewiki-latest-pages-articles1*", ).each do |file|
+  content = []
+  File.open(file, "r:UTF-8") do |f|
+    file_content = f.read
+    file_content = file_content.gsub(/\n/, ""); nil                       # clean the line breaks
+    file_content.scan(/{{-start-}}(.*?){{-stop-}}/).each do |article|     #between each -start- -stop- is one article
+      content << article.to_s
+    end;
+  end;
+  puts content.size
+
+  # File.open("test", "w+") do |f|
+  #   content.each { |element| f.puts(element) }
+  # end; nil
+
+  # get the categories from the categories-files
+  categories = []
+  File.readlines('../data/categories').each do |line|
+    line = line.gsub(/\n/, "")
+    categories << line
+  end; nil
+
+  matched_articles = []
+
+
+  # extract the articles which match with one category
+  content.each do |article|
+    catch :categoryfound do
+      categories.each do |category|
+        if article.include?("[[Kategorie:#{category}]]")
+          matched_articles << article
+        end
+      end
+    end
+  end; nil
+
+  # clean the matches_articles from wikipedia markups
+  matched_articles.map! do |element|
+    element = element.gsub(/<ref(.*?)<\/ref>/, "")
+    element = element.gsub(/== Literatur ==(.*?)==/, "")
+    element = element.gsub(/== Weblinks ==(.*?)==/, "")
+    element = element.gsub(/== Einzelnachweise ==(.*?)==/, "")
+    element = element.gsub(/\[\[/, "")
+    element = element.gsub(/\]\]/, "")
+    element = element.gsub(/<ref(.*?)>/, "")
+    element = element.gsub(/\'\'{{lang(.*?)\'\'/, "")
+    element = element.gsub(/{{lang(.*?)}}/, "")
+    element = element.gsub(/=====/, "")
+    element = element.gsub(/====/, "")
+    element = element.gsub(/===/, "")
+    element = element.gsub(/==/, "")
+    element = element.gsub(/'''''/, "")
+    element = element.gsub(/''''/, "")
+    element = element.gsub(/'''/, "")
+    element = element.gsub(/''/, "")
+    element = element.gsub(/\*\*\*\*\*/, "")
+    element = element.gsub(/\*\*\*\*/, "")
+    element = element.gsub(/\*\*\*/, "")
+    element = element.gsub(/\*\*/, "")
+    element = element.gsub(/\{\|(.*?)\|\}/, "")
+    element = element.gsub(/\{\{(.*?)\}\}/, "")
+    element = element.gsub(/&nbsp\;/, "")
+  end;nil
+
+  # export the clean articles to a new file
+  File.open("../data/clean_extract" + "#{counter}", "w") do |f|
+    matched_articles.each do |article|
+      f.write article
+    end
+    counter += 1
+  end
+
+end; nil
