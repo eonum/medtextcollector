621b4ea1c0a5e99d7a57f494a84fc918807644f3
Arun Sittampalam <arun@sittampalam.ch>
2017-07-21 15:04:24 +0200

    Extract documents by TLD.

--- postprocess.py
+++ postprocess.py
@@ -1,5 +1,6 @@
 import os
 import tldextract
+import shutil
 
 def extract_urls(path):
     urls = []
@@ -11,13 +12,27 @@ def extract_urls(path):
                 urls.append(url)
         break # no subdirectories
     return urls            
-                
+
+
+def extract_tld(url):
+    tld = tldextract.extract(url)
+    tld = tld.domain + '.' + tld.suffix
+    return tld
+
+
 def extract_tlds(path):
-    urls = extract_urls(path)
-    tlds = set()
-    for url in urls:    
-        tld = tldextract.extract(url)
-        tld = tld.domain + '.' + tld.suffix
-        tlds.add(tld)
-    return tlds
+    return {extract_tld(url) for url in extract_urls(path)}
     
+
+def extract_documents_by_tlds(tlds, input_path, output_path):
+    for (dirpath, dirnames, filenames) in os.walk(input_path):
+        for filename in filenames: 
+            with open(os.path.join(input_path, filename), 'r') as file:
+                header = file.readline()
+                url = header.split(';')[0].strip('"')
+                if extract_tld(url) in tlds:
+                    print('Copying ' + filename)
+                    shutil.copy2(os.path.join(dirpath, filename), output_path)
+        break # no subdirectories
+    
+
