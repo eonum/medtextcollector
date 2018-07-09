bab39ce2a9d6f044029089575fe985f8bc3b4907
Tim Peter <tim.peter@eonum.ch>
2018-07-09 11:43:04 +0200

    Fix URL filter.

--- data_analysis_toolkit.py
+++ data_analysis_toolkit.py
@@ -45,4 +45,4 @@ def get_files_from_folder(path):
 
 def extract_sentences(content, sentence_detector):
     return [sentence.replace('\n', ' ') for sentence in sentence_detector.tokenize(content) if not sentence.isspace()
-           and sentence[:4] != 'http' and len(sentence) > 12 and 4 * sum(c.isdigit() for c in sentence) < len(sentence)]
+           and sentence[:5] != '"http' and len(sentence) > 12 and 4 * sum(c.isdigit() for c in sentence) < len(sentence)]
